c28cdf2fe5b2c0050f1e45e26178162afa6a8bb5
Allen Garvey <allen.garvey@gmail.com>
2025-12-20 17:18:12 -0500

    Refactor current album to store album index

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -16,11 +16,13 @@ export const Page = ({ homeUrl, albums }: Props) => {
     const [playState, setPlayState] = useState<PlayState>('IS_EMPTY');
     const [elapsedTime, setElapsedTime] = useState(0);
     const [currentTrackIndex, setCurrentTrackIndex] = useState(-1);
-    const [currentAlbum, setCurrentAlbum] = useState<Album | null>(null);
+    const [currentAlbumIndex, setCurrentAlbumIndex] = useState(-1);
     const [audioVolume, setAudioVolume] = useState(getUserVolume());
     const [canPlayOpus, setCanPlayOpus] = useState(true);
     const audio = useRef(new Audio());
-    const currentTrack = currentAlbum?.tracks[currentTrackIndex];
+    const currentAlbum: Album | undefined = albums[currentAlbumIndex];
+    const currentTrack: Track | undefined =
+        currentAlbum?.tracks[currentTrackIndex];
 
     useEffect(() => {
         audio.current.addEventListener('loadeddata', () => {
@@ -52,9 +54,9 @@ export const Page = ({ homeUrl, albums }: Props) => {
     };
     const onButtonClicked = () => {};
 
-    const albumPlayButtonClicked = (album: Album) => {
-        if (!currentAlbum || currentAlbum.meta.slug !== album.meta.slug) {
-            setCurrentAlbum(album);
+    const albumPlayButtonClicked = (albumIndex: number) => {
+        if (!currentAlbum || currentAlbumIndex === albumIndex) {
+            setCurrentAlbumIndex(albumIndex);
             setCurrentTrackIndex(0);
             setPlayState('IS_LOADING');
         } else {
@@ -66,14 +68,14 @@ export const Page = ({ homeUrl, albums }: Props) => {
         }
     };
 
-    const trackPlayButtonClicked = (trackIndex: number, album: Album) => {
+    const trackPlayButtonClicked = (trackIndex: number, albumIndex: number) => {
         if (
             !currentTrack ||
-            currentAlbum?.meta.slug !== album.meta.slug ||
+            currentAlbumIndex === albumIndex ||
             trackIndex !== currentTrackIndex
         ) {
             setCurrentTrackIndex(trackIndex);
-            setCurrentAlbum(album);
+            setCurrentAlbumIndex(albumIndex);
             setPlayState('IS_LOADING');
         } else {
             if (playState === 'IS_PAUSED') {
@@ -92,7 +94,7 @@ export const Page = ({ homeUrl, albums }: Props) => {
             <p className={style.description}>
                 Selected musical compositions and recordings
             </p>
-            {albums.map(album => (
+            {albums.map((album, i) => (
                 <AlbumComponent
                     album={album}
                     isPlaying={
@@ -100,9 +102,9 @@ export const Page = ({ homeUrl, albums }: Props) => {
                         playState !== 'IS_PAUSED'
                     }
                     onTrackPlayRequested={(trackIndex: number) => {
-                        trackPlayButtonClicked(trackIndex, album);
+                        trackPlayButtonClicked(trackIndex, i);
                     }}
-                    onAlbumPlayRequested={() => albumPlayButtonClicked(album)}
+                    onAlbumPlayRequested={() => albumPlayButtonClicked(i)}
                     currentTrack={currentTrack}
                     key={album.meta.title}
                 />
